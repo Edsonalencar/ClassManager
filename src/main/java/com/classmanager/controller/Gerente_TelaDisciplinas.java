17a89eb5625462fbd3f42a6f8558d8ab28619a63
Karoline <karol.saraiva2014@gmail.com>
2023-10-15 20:09:30 -0300

    Controller Gerente

---
+++ src/main/java/com/classmanager/controller/Gerente_TelaDisciplinas.java
@@ -0,0 +1,26 @@
+package com.classmanager.controller;
+
+import com.classmanager.view.Telas;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Gerente_TelaDisciplinas {
+	public void telaProfessor(ActionEvent event) throws Exception {
+		Telas.Gerente_TelaProfessores();
+	}
+	public void telaAluno(ActionEvent event) throws Exception {
+		Telas.Gerente_TelaAlunos();
+	}
+	public void telaInicio(ActionEvent event) throws Exception {
+		Telas.Gerente_TelaInicial();
+	}
+	public void telaTurma(ActionEvent event) throws Exception {
+		Telas.Gerente_TelaTurmas();
+	}
+	public void logout(ActionEvent event) throws Exception {
+		Telas.telaLogin();
+	}
+}
+ 
---
+++ src/main/java/com/classmanager/controller/Gerente_TelaTurmas.java
@@ -0,0 +1,27 @@
+package com.classmanager.controller;
+
+import com.classmanager.view.Telas;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Gerente_TelaTurmas {
+	
+	public void telaProfessor(ActionEvent event) throws Exception {
+		Telas.Gerente_TelaProfessores();
+	}
+	public void telaAluno(ActionEvent event) throws Exception {
+		Telas.Gerente_TelaAlunos();
+	}
+	public void telaInicio(ActionEvent event) throws Exception {
+		Telas.Gerente_TelaInicial();
+	}
+	public void telaDisciplina(ActionEvent event) throws Exception {
+		Telas.Gerente_TelaDisciplina();
+	}
+	public void logout(ActionEvent event) throws Exception {
+		Telas.telaLogin();
+	}
+
+}
