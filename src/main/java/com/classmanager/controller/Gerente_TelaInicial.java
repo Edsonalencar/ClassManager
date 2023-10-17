1f9f225520e9cb85f795722114f89ad7487e367c
jooj234 <firejohn479@gmail.com>
2023-10-16 23:33:04 -0300

    Tables Layout

--- src/main/java/com/classmanager/controller/Gerente_TelaInicial.java
+++ src/main/java/com/classmanager/controller/Gerente_TelaInicial.java
@@ -1,14 +1,46 @@
 package com.classmanager.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import com.classmanager.view.Telas;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class Gerente_TelaInicial {
+public class Gerente_TelaInicial implements Initializable {
 	@FXML private TextField CampoBuscar;
 	@FXML private TextField CampoSelecionarProfessor;
+	
+	@FXML
+    private TableView TabelaItens;
+    @FXML
+    private TableColumn ColunaTurma;
+    @FXML
+    private TableColumn ColunaDisciplina;
+    @FXML
+    private TableColumn ColunaProfessor;
+    @FXML
+    private TableColumn ColunaAulas;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ColunaTurma.setCellValueFactory(
+                new PropertyValueFactory<>("ID Turma"));
+        ColunaDisciplina.setCellValueFactory(
+                new PropertyValueFactory<>("Disciplina"));
+        ColunaProfessor.setCellValueFactory(
+                new PropertyValueFactory<>("Professor"));
+        ColunaAulas.setCellValueFactory(
+                new PropertyValueFactory<>("Aulas"));
+        
+        TabelaItens.setItems(null); //ADICIONAR DE UM VETOR CONTENDO OBJETOS COM (IDTURMA(LONG),DISCIPLINA,PROFESSOR(1º DO ARRAY,TIMETABLE)
+    }
 	
 	public void autenticar(ActionEvent event) throws Exception {
 		
--- src/main/java/com/classmanager/controller/Gerente_TelaProfessores.java
+++ src/main/java/com/classmanager/controller/Gerente_TelaProfessores.java
@@ -1,12 +1,21 @@
 package com.classmanager.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import com.classmanager.view.Telas;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class Gerente_TelaProfessores {
+public class Gerente_TelaProfessores   {
+	
+	
 	
 	public void telaDisciplina(ActionEvent event) throws Exception {
 		Telas.Gerente_TelaDisciplina();
